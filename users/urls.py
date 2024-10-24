fbf6402374e64d129de2173ecf2ca3540329466e
NoobikNovi4ok <antonpetya1330@mail.ru>
2024-10-24 23:57:52 +0300

    СВО

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from users.views import login, registration, users_basket
+from users.views import login, registration, users_basket, profile, logout, delete_order
 
 
 app_name = "users"
@@ -8,4 +8,7 @@ urlpatterns = [
     path("login/", login, name="login"),
     path("registration/", registration, name="registration"),
     path("users-basket", users_basket, name="users-basket"),
+    path("profile/", profile, name="profile"),
+    path("logout/", logout, name="logout"),
+    path('delete_order/<int:order_id>/', delete_order, name='delete_order')
 ]
--- users/views.py
+++ users/views.py
@@ -4,7 +4,10 @@ from django.contrib import auth
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from orders.models import Order, OrderItem
+from django.db.models import Prefetch
+from django.shortcuts import get_object_or_404, redirect
 
 def registration(request):
     if request.method == "POST":
@@ -56,6 +59,39 @@ def login(request):
 
     return render(request, "users/login.html", {"form": form, "title": "Авторизация"})
 
+@login_required
+def profile(request):
+    orders = (
+        Order.objects.filter(user=request.user)
+        .prefetch_related(
+            Prefetch(
+                "orderitem_set",
+                queryset=OrderItem.objects.select_related("product"),
+            )
+        )
+        .order_by("-id")
+    )
+
+    for order in orders:
+        order.total_price = sum(item.products_price() for item in order.orderitem_set.all())
+
+    context = {"title": "Профиль пользователя",  "orders": orders }
+
+    return render(request, "users/profile.html", context)
+
+def delete_order(request, order_id):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+    order.delete()
+    messages.add_message(
+                    request, messages.INFO, f"{request.user} вы успешно удалили заказ"
+                )
+    return HttpResponseRedirect(reverse("users:profile"))
 
 def users_basket(request):
     return render(request, "users/users_basket.html")
+
+@login_required
+def logout(request):
+    auth.logout(request)
+
+    return HttpResponseRedirect(reverse("home"))
